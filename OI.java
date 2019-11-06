21ab2cbc93c347a76aabc0b1095d30997c70480a
minevsr0bl0x <57153112+minevsr0bl0x@users.noreply.github.com>
2019-11-06 16:04:08 -0500

    Update OI.java

--- OI.java
+++ OI.java
@@ -1,10 +1,15 @@
 import yes.nanobotMap;
 public class OI {
-public static final int JOYSTICK = 0;
+public static final int JOYSTICK = 2;
 
-public static final int BUTTON = 1
+public static final int BUTTON_1 = 1
 
 static Joystick = new joystick;
 
+  public final Button autoforwardbutton - new JoystickButton(Joystick BUTTON_1);
+}
+
 public OI() {
+  autoforwardbutton.whenPrressed(new AutoForward(3.3,0)):
+}
 
