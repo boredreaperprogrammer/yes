e70aa450fe821a2ba6001e3ba7c2e7f740cadd3f
minevsr0bl0x <57153112+minevsr0bl0x@users.noreply.github.com>
2019-11-11 15:57:29 -0500

    Update OI.java

--- OI.java
+++ OI.java
@@ -4,16 +4,20 @@ public static final int JOYSTICK = 2;
 
 public static final int BUTTON_1 = 1;
 public static final int BUTTON_2 = 3;
+public static final int BUTTON_3 = 4;
   
 static Joystick = new Joystick;
 
   public final Button autoforwardbutton = new JoystickButton(Joystick BUTTON_1);
   public final Button turnonbutton = new JoystickButton(Joystick BUTTON_2);
+  public final Button turncamonbutton = new JoystickButton(Joystick BUTTON_3);
 
 
 public OI() {
   autoforwardbutton.whenPressed(new AutoForward(3.3,0)):
   turnonbutton.whenPressed (new turnon);
+  turncamonbutton.whenpressed(new turncamon);
+  
 }
 public static Joystick = getJoystick() {
   return joystick;
