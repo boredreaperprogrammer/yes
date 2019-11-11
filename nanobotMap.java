047b79485f86da8a689c63a79751f617ea8f11a1
minevsr0bl0x <57153112+minevsr0bl0x@users.noreply.github.com>
2019-11-11 15:52:09 -0500

    Update nanobotMap.java

--- nanobotMap.java
+++ nanobotMap.java
@@ -1,5 +1,7 @@
 public class nanobotMap {
 public static final int NANO_MOTOR_PORT = 1;
+public static final int CAM_PORT = 2;
 
 public static Nanomotor = new Nanomotor(NANOMOTOR_PORT);
+public static Camera = new Camera(CAM_PORT);
 }
